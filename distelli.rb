e9703db16138b3e6d9c2c21a80196e145bfb7979
Chris Mosetick <cmosetick@gmail.com>
2016-04-06 10:13:42 -0700

    add brew formula

---
+++ distelli.rb
@@ -0,0 +1,20 @@
+class DistelliCli < Formula
+  desc "Distelli CLI - Build, Ship and Deploy code faster"
+  homepage "https://www.distelli.com/docs/agent/installing-the-distelli-cli"
+  url "https://s3.amazonaws.com/download.distelli.com/distelli.Darwin-x86_64/distelli.Darwin-x86_64-3.63.4.gz"
+  version "3.63.4"
+  sha256 "d73a5388b12b72c088100a0f276ee9b10b6b74e46bfe5d12478e63e19cd65676"
+
+def install
+        bin.install "distelli.Darwin-x86_64-3.63.4"
+        system "mv", bin/"distelli.Darwin-x86_64-3.63.4", bin/"distelli"
+end
+
+ohai "To use distelli cli to start application builds make sure that you have
+      already created a free account on the Distelli web site:
+      https://www.distelli.com/login"
+
+test do
+    system "#{bin}/distelli", "version"
+  end
+end
